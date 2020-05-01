c7a749fb1e4c615d17652ed0844bd773d7d1a5a5
kyongsoo <59049565+kyongsoo@users.noreply.github.com>
2020-05-02 03:02:09 +0900

    update: css

--- js/clock.js
+++ js/clock.js
@@ -9,8 +9,7 @@ function getTime() {
     const seconds = date.getSeconds();
 
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}`: seconds}`;
+        minutes < 10 ? `0${minutes}` : minutes}`;
 }
 
 function init() {
