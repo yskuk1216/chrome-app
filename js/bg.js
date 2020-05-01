cc3887aa0c8ba0955462b9fb356fddcc30bec31b
kyongsoo <59049565+kyongsoo@users.noreply.github.com>
2020-05-02 01:29:28 +0900

    update: background

---
+++ js/bg.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const img_number = 3;
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `../images/${imgNumber + 1}.jpg`;
+    image.classList.add("bigImage");
+    body.prepend(image);
+}
+
+function getRandom() {
+    const number = Math.floor(Math.random() * img_number);
+    return number;
+}
+
+function init() {
+    const randomNumber = getRandom();
+    paintImage(randomNumber);
+}
+
+init();
