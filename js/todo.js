71b29e07314c1ba720faf6bbef35dba55ac7305b
kyongsoo <59049565+kyongsoo@users.noreply.github.com>
2020-05-01 13:35:56 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -4,16 +4,31 @@ const todoList = document.querySelector(".js-todoList");
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    span.innerText = text
     const delBtn = document.createElement("button");
+    const newID = toDos.length + 1;
+
+    span.innerText = text;
     delBtn.innerText = "X";
 
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newID
     todoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newID
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -24,9 +39,12 @@ function handleSubmit(event) {
 }
 
 function loadToDo() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function (toDo) {
+            paintToDo(toDo.text);
+        })
     } else {
 
     }
